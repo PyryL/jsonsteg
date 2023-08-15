4184172bb42f45ab73cd78e086321fe68c089e60
PyryL <pyry@pyry.info>
2023-08-15 16:45:23 +0300

    added building and publishing invoke tasks

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from invoke import task
+import os
 
 @task
 def test(ctx):
@@ -11,3 +12,19 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
+
+# By removing the build directory before publish commands
+# interactive question made by Poetry can be avoided
+def remove_build_directory(ctx):
+    ctx.run("rm -rf dist/", pty=True)
+
+@task
+def publish(ctx):
+    remove_build_directory(ctx)
+    ctx.run("poetry publish --build", pty=True)
+
+@task
+def publish_test(ctx):
+    remove_build_directory(ctx)
+    ctx.run("poetry config repositories.test-pypi https://test.pypi.org/legacy/", pty=True)
+    ctx.run("poetry publish --build --repository test-pypi", pty=True)
